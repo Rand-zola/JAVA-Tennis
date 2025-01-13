4feae1a2a6e1dcc07f9078cdc34db27367a4fc43
Rakotoarisoa Zolalaina <zolalainarakotoarisoa5@gmail.com>
2025-01-13 20:03:45 +0300

    Modification classe joueur

--- src/tennis/Joueur.java
+++ src/tennis/Joueur.java
@@ -3,7 +3,7 @@ package tennis;
 import java.awt.*;
 
 public class Joueur {
-	private int x, y;         
+    private int x, y;        
     private final int WIDTH = 20, HEIGHT = 100; 
     private final int SPEED = 10; 
 
@@ -12,24 +12,28 @@ public class Joueur {
         this.x = startX;
         this.y = startY;
     }
+
     // Déplacer la raquette vers le haut
     public void moveUp() {
-        if (y > 0) {  
+        if (y > 50) {  
             y -= SPEED; 
         }
     }
 
     // Déplacer la raquette vers le bas
     public void moveDown() {
-        if (y < 460 - HEIGHT) {  
+        if (y < 450 - HEIGHT) {  
             y += SPEED; 
         }
     }
+
+    // Dessiner la raquette
     public void draw(Graphics g) {
         g.setColor(Color.WHITE);  
         g.fillRect(x, y, WIDTH, HEIGHT);  
     }
-    // Retourner le rectangle de la raquette pour la gestion des collisions
+
+    // Retourne le rectangle qui représente la raquette
     public Rectangle bord() {
         return new Rectangle(x, y, WIDTH, HEIGHT);
     }
